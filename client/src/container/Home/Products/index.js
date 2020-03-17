40acf35a4f29145dcd3630f61c41cd932a9f33ad
Avazkhon <kamalxanovavazxon@gmail.com>
2020-03-17 22:42:06 +0700

    update Products

--- client/src/container/Home/Products/index.js
+++ client/src/container/Home/Products/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
 
 import {
   Container,
@@ -8,63 +7,24 @@ import {
   Col,
 } from 'react-bootstrap';
 
-// import {s
-
 import ListProducts from './ListProducts';
 
-class Products extends React.Component {
-  constructor(props) {
-    super(props);
-
-  }
-
-  // componentDidMount() {
-  //   const { getProducts, getProductsByIdCategory, idCategory } = this.props;
-  //   if (idCategory) {
-  //     getProductsByIdCategory(idCategory)
-  //   } else {
-  //     getProducts();
-  //   }
-  // }
-
-  render() {
-    const {
-      products,
-    } = this.props;
-    return (
-      <Container>
-        <Row>
-          <Col>
-            <ListProducts
-              products={products && products.products}
-            />
-          </Col>
-        </Row>
-      </Container>
-    );
-  }
-}
+const Products = ({
+  products
+}) => (
+  <Container>
+    <Row>
+      <Col>
+        <ListProducts
+          products={products && products.products}
+        />
+      </Col>
+    </Row>
+  </Container>
+);
 
 Products.propType = {
-  // products: PropTypes.shape({}),
-  auth: PropTypes.shape({}),
-
-  // getProducts: PropTypes.func,
-  // getProductsByIdCategory: PropTypes.func,
+  products: PropTypes.shape({}),
 }
 
-function mapStateToProps(state) {
-  const {
-    auth,
-    // products,
-  } = state;
-  return {
-    auth,
-    // products,
-  };
-}
-
-export default connect(mapStateToProps, {
-  // getProducts,
-  // getProductsByIdCategory,
-})(Products);
+export default Products;
